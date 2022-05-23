72af38879a6ed9d94adc79eed4cdd4a05f60bed3
Ehayward <104617060+Ehayward@users.noreply.github.com>
2022-05-24 00:21:07 +1000

    updated empties of coins to be at 0,2,0 and 1,2,0

--- Assets/Flip.cs
+++ Assets/Flip.cs
@@ -6,6 +6,7 @@ public class Flip : MonoBehaviour
 {
     static Rigidbody coin;
     public static Vector3 coinVelocity;
+    bool isFlipping = false;
     
 
     void Start()
@@ -24,9 +25,11 @@ public class Flip : MonoBehaviour
             float dirX = Random.Range(0, 500);
             float dirY = Random.Range(0, 500);
             float dirZ = Random.Range(0, 500);
+
             //This is the coin's starting position and rotation.
             transform.position = new Vector3(0, 2, 0);
             transform.rotation = Quaternion.identity;
+
             //This sets the force that it will send the coin upward.
             coin.AddForce(transform.up * 500);
             coin.AddTorque(dirX, dirY, dirZ);
