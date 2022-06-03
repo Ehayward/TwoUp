bcfb67c497176d79d0d37df5ad23511c6aa7ac8b
Ehayward <104617060+Ehayward@users.noreply.github.com>
2022-06-03 16:55:06 +1000

    Added pictures for buttons, added additional  Function in BettingManager

--- Assets/Scripts/BettingGameManager.cs
+++ Assets/Scripts/BettingGameManager.cs
@@ -5,13 +5,16 @@ using UnityEngine.UI;
 
 public class BettingGameManager : MonoBehaviour
 {
+    //variable names
     private int playerCashStack;
     private int playerChoice;
     private int coinResults;
 
+    //Text names
     public Text cashStackAmount;
     public Text playerFeedbackText;
 
+    //Button names
     public Button bothTailsButton;
     public Button bothHeadsButton;
     public Button oneOfEachButton;
@@ -20,8 +23,19 @@ public class BettingGameManager : MonoBehaviour
     public Button increaseBetButton;
     public Button decreaseBetButton;
 
+    //Canvas names
     public GameObject bettingChoicesCanvas;
     public GameObject flipCoinsandResultsCanvas;
+
+    //Camera names
+    public GameObject Coin1;
+    public GameObject Coin2;
+    public GameObject thirdPersonCamera;
+    public GameObject mainMenuCamera;
+
+    //Group Objects
+    public GameObject threeBettingButtons;
+
 
     void Start()
     {
@@ -30,15 +44,36 @@ public class BettingGameManager : MonoBehaviour
 
     private void NewGame()
     {
+        //setting variable values
         playerCashStack = 25;
-        playerChoice = 0;
 
+        //selecting canvas and camera
         flipCoinsandResultsCanvas.gameObject.SetActive(false);
+        Coin1.gameObject.SetActive(false);
+        Coin2.gameObject.SetActive(false);
+        thirdPersonCamera.SetActive(false);
+
         bettingChoicesCanvas.gameObject.SetActive(true);
-        
-        playerFeedbackText.text = "Welcome to Two-Up! What result do you think the coins will land on?";
+        mainMenuCamera.SetActive(true);
+        threeBettingButtons.gameObject.SetActive(true);
+               
+        playerFeedbackText.text = "Welcome to Two-Up!\n What result do you think the coins will land on?";
 
+    }
 
+    private void BetAgain()
+    {
+        //selecting canvas and camera
+        flipCoinsandResultsCanvas.gameObject.SetActive(false);
+        Coin1.gameObject.SetActive(false);
+        Coin2.gameObject.SetActive(false);
+        thirdPersonCamera.SetActive(false);
+
+        bettingChoicesCanvas.gameObject.SetActive(true);
+        mainMenuCamera.SetActive(true);
+        threeBettingButtons.gameObject.SetActive(true);
+
+        playerFeedbackText.text = "Welcome back, how about another bet!\n What result do you think the coins will land on?";
 
     }
 
